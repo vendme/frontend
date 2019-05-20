4d2851c8967c4885955f38185f071902e707bdf5
CatsAreMLG <joshuadgon@gmail.com>
2019-05-20 16:42:14 -0500

    Refactor AddStalls component

--- src/components/marketedit/MarketEditss.js
+++ src/components/marketedit/MarketEditss.js
@@ -1,83 +1,91 @@
 import React, { Component } from 'react'
-import AddStall from "../addstalls/AddStall";
-import styles from "./marketedit.module.css";
+import AddStall from '../addstalls/AddStall'
+import styles from './marketedit.module.css'
 
 class MarketEdit extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: null,
-      addStallList: [],
-      submittedStallList: [],
-      quantity: null,
-      size: null,
-      name: "",
-      address: ""
-    };
+  state = {
+    submittedStallList: [],
+    quantity: '',
+    width: '',
+    length: ''
   }
 
   changeHandler = event => {
-    event.preventDefault();
+    event.preventDefault()
     this.setState({ [event.target.name]: event.target.value })
-
-    console.log(this.state)
   }
-  
-  addStall = event => {
-    event.preventDefault();
-    const idValue = Math.floor(Math.random()*10);
-
-    if(this.state.quantity || this.state.size){
-      this.submitStallToAdd();
-    }
-    else {
-      const addNewStall = [];
-      addNewStall.push(this.state.addStallList);
-      addNewStall.push(<AddStall  key={idValue} id={idValue} mystate={this.state} changeHandler={this.changeHandler} submitStallToAdd={this.submitStallToAdd} removeStall={this.removeStall}/>);
-      this.setState({ addStallList: addNewStall });
-    }
-  };
 
   submitStallToAdd = () => {
-
-    const updatedList = this.state.submittedStallList;
-    const add = {
-      id: this.state.id,
-      quantity: this.state.quantity,
-      size: this.state.size,
-      name: this.state.name,
-      address: this.state.address
+    if (this.state.quantity && this.state.width && this.state.length) {
+      const updatedList = this.state.submittedStallList
+      const add = {
+        quantity: this.state.quantity,
+        width: this.state.width,
+        length: this.state.length
+      }
+      updatedList.push(add)
+      this.setState({
+        submittedStallList: updatedList,
+        quantity: '',
+        width: '',
+        length: ''
+      })
     }
-    console.log("This worked")
-    updatedList.push(add);
-    this.setState({ submittedStallList: updatedList })
-    this.setState({ quantity: "", size: "", })
-    // this.state.props.update(this.state);
   }
   render() {
-    return ( 
+    return (
       <div>
-        <form action="" id="from1">
+        <form action="" id="form1">
           <label>
             Market Name
-            <input type="text" name="name" value={this.state.name} onChange={this.changeHandler}/>
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.changeHandler}
+            />
           </label>
           <label>
             Address
-            <input type="text" name="address" value={this.state.address} onChange={this.changeHandler}/>
+            <input
+              type="text"
+              name="address"
+              value={this.state.address}
+              onChange={this.changeHandler}
+            />
           </label>
         </form>
-        <h3>Available Stalls</h3>
+        <form action="">
+          <h3>Available Stalls</h3>
+          <AddStall
+            mystate={this.state}
+            changeHandler={this.changeHandler}
+            submitStallToAdd={this.submitStallToAdd}
+            removeStall={this.removeStall}
+            quantity={this.state.quantity}
+            width={this.state.width}
+            length={this.state.length}
+          />
+          <div className={styles.addContainer}>
+            <h3>Add Stall</h3>
+            <button
+              type="button"
+              className={styles.add}
+              onClick={this.submitStallToAdd}>
+              +
+            </button>
+          </div>
+        </form>
         <div>
-          {this.state.addStallList}
+          {this.state.submittedStallList.map(stall => (
+            <div>
+              Quantity: {stall.quantity}
+              Size: {`${stall.width} x ${stall.length}`}
+            </div>
+          ))}
         </div>
-        <div className={styles.addContainer}>
-          <h3>Add Stalls</h3>
-          <button type="button" className={styles.add} onClick={this.addStall}>+</button>
-        </div>
-        <input type="button" form="form1" onClick={this.submitStallToAdd}/>
       </div>
-    );
+    )
   }
 }
 
