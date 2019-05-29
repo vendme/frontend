23570e77a5ad4db2f0cff00f835b7d08dd70429a
CatsAreMLG <joshuadgon@gmail.com>
2019-05-28 22:21:13 -0500

    Test more IOS fixes

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -14,7 +14,81 @@ import MenuIcon from '@material-ui/icons/Menu'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import { mainListItems, secondaryListItems } from './navlist/NavItems'
-import styles from './navbar.styles.js'
+// import styles from './navbar.styles.js'
+
+let drawerWidth = 240
+
+const styles = theme => {
+  return {
+    root: {
+      display: 'flex'
+    },
+    toolbar: {
+      paddingRight: 24, // keep right padding when drawer closed
+      backgroundColor: theme.palette.primary.dark
+    },
+    toolbarIcon: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      padding: '0 8px',
+      ...theme.mixins.toolbar
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      })
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen
+      })
+    },
+    menuButton: {
+      marginLeft: 12,
+      marginRight: 36
+    },
+    menuButtonHidden: {
+      display: 'none'
+    },
+    title: {
+      flexGrow: 1,
+      fontFamily: 'Pacifico',
+      fontSize: 30
+    },
+    drawerPaper: {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen
+      })
+    },
+    drawerPaperClose: {
+      overflowX: 'hidden',
+      transition: theme.transitions.create(['width', 'opacity', 'height'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      }),
+      width: 0,
+      height: 0,
+      opacity: 0
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing.unit * 3,
+      height: '100vh',
+      overflow: 'auto'
+    }
+  }
+}
 
 class NavBar extends React.Component {
   state = {
