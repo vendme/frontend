1332db1774992e687e1c96d331610726e8d6ef2f
CatsAreMLG <joshuadgon@gmail.com>
2019-06-24 17:21:22 -0500

    Add error snackbar

--- src/components/signup/SignUp.js
+++ src/components/signup/SignUp.js
@@ -1,76 +1,96 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import SignUpForm from './SignUpForm.js'
+
 import {
-  Button,
-  Link,
-  CssBaseline,
-  FormControl,
-  FormControlLabel,
-  Checkbox,
-  Input,
-  InputLabel,
-  Paper,
-  Typography,
-  withStyles
+  Snackbar,
+  SnackbarContent,
+  IconButton,
+  CssBaseline
 } from '@material-ui/core'
 
-import gooogleButton2 from './btn_google_signin_light_normal_web@2x.png'
+import { Error, Close } from '@material-ui/icons'
+import { red } from '@material-ui/core/colors'
+class SignUp extends React.Component {
+  state = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+    open: false
+  }
 
-import styles from './signup.styles'
-
-function SignUp(props) {
-  const { classes } = props
-  return (
-    <main className={classes.main}>
-      <CssBaseline />
-      <Paper className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form}>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="email">Email Address</InputLabel>
-            <Input id="email" name="email" autoComplete="email" autoFocus />
-          </FormControl>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <Input
-              name="password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-          </FormControl>
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
+  onClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return
+    }
+    this.setState({ open: false })
+  }
+  onSubmit = e => {
+    e.preventDefault()
+    const isInvalid =
+      this.state.passwordOne !== this.state.passwordTwo ||
+      this.state.passwordOne === '' ||
+      this.state.email === '' ||
+      this.state.username === ''
+    if (!isInvalid) {
+    } else {
+      this.setState({
+        error: { message: 'Passwords must match!' },
+        open: true
+      })
+    }
+  }
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+  render() {
+    return (
+      <>
+        <CssBaseline />
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'center'
+          }}
+          open={this.state.open}
+          autoHideDuration={6000}
+          onClose={this.onClose}
+          style={{ margin: '3.5rem 0', left: 'calc(50% + 27px)' }}>
+          <SnackbarContent
+            aria-describedby="client-snackbar"
+            message={
+              <span
+                id="client-snackbar"
+                style={{
+                  display: 'flex',
+                  alignItems: 'center'
+                }}>
+                <Error
+                  style={{
+                    fontSize: 20,
+                    opacity: 0.9,
+                    marginRight: '8px'
+                  }}
+                />
+                {this.state.error && this.state.error.message}
+              </span>
+            }
+            style={{ backgroundColor: red[700] }}
+            action={[
+              <IconButton
+                key="close"
+                aria-label="Close"
+                color="inherit"
+                onClick={this.onClose}>
+                <Close style={{ fontSize: 20 }} />
+              </IconButton>
+            ]}
           />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}>
-            Sign up
-          </Button>
-          <Link
-            href="http://vendme.herokuapp.com/auth/google"
-            variant="body2"
-            className={classes.google}>
-            <img
-              src={gooogleButton2}
-              className={classes.googleButton}
-              alt="Google Sign In"
-            />
-          </Link>
-        </form>
-      </Paper>
-    </main>
-  )
+        </Snackbar>
+        <SignUpForm onChange={this.onChange} onSubmit={this.onSubmit} />
+      </>
+    )
+  }
 }
-
-SignUp.propTypes = {
-  classes: PropTypes.object.isRequired
-}
-
-export default withStyles(styles)(SignUp)
+export default SignUp
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 import App from './App'
-import Firebase, { FirebaseContext } from './components/Firebase'
+import Firebase, { FirebaseContext } from './components/firebase'
 
 import './index.css'
 
 ReactDOM.render(
   <BrowserRouter>
     <HashRouter>
-      <FirebaseContext.Provider value={new Firebase()}>
-        <App />
-      </FirebaseContext.Provider>
+      {/* <FirebaseContext.Provider value={new Firebase()}> */}
+      <App />
+      {/* </FirebaseContext.Provider> */}
     </HashRouter>
   </BrowserRouter>,
   document.getElementById('root')
