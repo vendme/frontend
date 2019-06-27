bfae1a2e15fc79f77c0d053e3fa58a1f7cc38a57
CatsAreMLG <joshuadgon@gmail.com>
2019-06-26 21:25:36 -0500

    Add account component

---
+++ src/components/account/Account.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { PasswordForgetForm } from '../passwordforget'
+import PasswordChangeForm from '../passwordchange'
+
+const AccountPage = () => (
+  <div>
+    <h1>Account Page</h1>
+    <PasswordForgetForm />
+    <PasswordChangeForm />
+  </div>
+)
+
+export default AccountPage
