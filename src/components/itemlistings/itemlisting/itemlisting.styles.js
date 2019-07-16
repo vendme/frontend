d0e62ab84151ab69e175d6ca63830f122acaa127
CatsAreMLG <joshuadgon@gmail.com>
2019-07-15 20:48:03 -0500

    Add individual products page

---
+++ src/components/itemlistings/itemlisting/itemlisting.styles.js
@@ -0,0 +1,36 @@
+const styles = ({ spacing, palette, transitions, breakpoints }) => {
+  return {
+    root: {
+      display: 'flex',
+      alignItems: 'flex-start',
+      justifyContent: 'center',
+      flexDirection: 'row',
+      flexWrap: 'wrap'
+    },
+    item: {
+      width: 300,
+      maxWidth: 300,
+      margin: spacing(1)
+    },
+    cover: {
+      paddingTop: '100%',
+      width: '100%',
+      maxHeight: '100%',
+      marginRight: spacing(2),
+      marginBottom: spacing(1),
+      [breakpoints.down(800)]: {
+        height: 'auto',
+        width: 'auto',
+        minWidth: 120
+      }
+    },
+    name: {
+      margin: spacing(1)
+    },
+    price: {
+      fontWeight: 'bold',
+      margin: spacing(1)
+    }
+  }
+}
+export default styles
