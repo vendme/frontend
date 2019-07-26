a97c4982b08f46a852fe6e5c72efc84f773ef378
CatsAreMLG <joshuadgon@gmail.com>
2019-07-25 21:47:47 -0500

    Add item listing styling

--- src/components/itemlistings/itemlisting/itemlisting.styles.js
+++ src/components/itemlistings/itemlisting/itemlisting.styles.js
@@ -1,16 +1,52 @@
-const styles = ({ spacing, palette, transitions, breakpoints }) => {
+const styles = ({ spacing, palette, breakpoints }) => {
+  console.log(palette)
   return {
     root: {
+      position: 'relative',
       display: 'flex',
-      alignItems: 'flex-start',
-      justifyContent: 'center',
-      flexDirection: 'row',
-      flexWrap: 'wrap'
+      flexDirection: 'column',
+      alignItems: 'center',
+      marginTop: spacing(-3)
     },
-    item: {
-      width: 300,
-      maxWidth: 300,
-      margin: spacing(1)
+    modal: {
+      position: 'absolute',
+      top: '50%',
+      left: '50%'
+      // transform: `translate(50%, -50%)`
+    },
+    blurBox: {
+      position: 'fixed',
+      width: '100%',
+      maxHeight: 350,
+      margin: 'auto',
+      overflow: 'hidden',
+      zIndex: -1
+    },
+    picturesContainer: {
+      background: palette.grey['900'],
+      backgroundSize: 'cover',
+      width: '100%',
+      height: 350,
+      maxWidth: '100%',
+      margin: 'auto',
+      filter: 'blur(8px) brightness(0.4)'
+    },
+    pictures: {
+      width: '100%',
+      maxWidth: '100%',
+      margin: 'auto',
+      marginBottom: spacing(3)
+    },
+    picture: {
+      width: 350,
+      height: 350,
+      maxWidth: 350,
+      margin: 'auto'
+    },
+    about: {
+      width: 650,
+      maxWidth: 650,
+      margin: 'auto'
     },
     cover: {
       paddingTop: '100%',
@@ -24,12 +60,28 @@ const styles = ({ spacing, palette, transitions, breakpoints }) => {
         minWidth: 120
       }
     },
-    name: {
-      margin: spacing(1)
+    description: {
+      // margin: spacing(1)
+    },
+    info: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center'
     },
     price: {
       fontWeight: 'bold',
       margin: spacing(1)
+    },
+    priceBox: {
+      fontSize: spacing(2),
+      fontWeight: 'bold',
+      border: '1px solid ' + palette.secondary.main,
+      borderRadius: 100,
+      padding: spacing(1),
+      marginRight: spacing(2)
+    },
+    divider: {
+      margin: spacing(2, 0, 2, 0)
     }
   }
 }
