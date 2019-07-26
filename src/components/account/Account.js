d61d9d73983b80dc286bd1c7f447a53b0fe90d48
CatsAreMLG <joshuadgon@gmail.com>
2019-07-25 21:48:03 -0500

    Fix cloudinary upload

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -1,23 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
+import { Button, withStyles } from '@material-ui/core'
+import { Attachment, Save } from '@material-ui/icons'
 
 import { AuthUserContext, withAuthorization } from '../session'
-// import { PasswordForgetForm } from '../passwordforget'
 import PasswordChangeForm from '../passwordchange'
+import styles from './account.styles.js'
+import tokenDateChecker from '../../services/tokenDateChecker'
 
-const AccountPage = () => {
+const AccountPage = ({ classes, history }) => {
+  const [file, setFile] = useState(null)
+  // const [user, setUser] = useState(null)
+  // useEffect(_ => {
+  //   ;(async function verifyData() {
+  //     if (tokenDateChecker()) {
+  //       const { data } = await Axios.get(
+  //         'https://vendme.herokuapp.com/auth/verify'
+  //       )
+  //       setUser(data)
+  //     } else {
+  //       history.push('/login')
+  //     }
+  //   })()
+  // })
+  const fileSelectedHandler = e => {
+    window.cloudinary.openUploadWidget(
+      {
+        cloud_name: 'vendme',
+        upload_preset: 'jdm3qonc',
+        tags: ['profile_pic']
+      },
+      function(error, result) {
+        if (result) {
+          setFile(result[0].secure_url)
+          Axios.put('https://vendme.herokuapp.com/api/users', {
+            profile_pic: result[0].secure_url
+          })
+            .then(res => {
+              console.log(res)
+            })
+            .catch(err => console.log(err.message))
+        }
+      }
+    )
+  }
   return (
-    <AuthUserContext.Consumer>
-      {authUser => (
-        <div>
-          <h1>Account: {authUser.email}</h1>
-          <h2>Account Type: {authUser.email}</h2>
-          <PasswordChangeForm />
+    <>
+      <AuthUserContext.Consumer>
+        {authUser => (
+          <div>
+            <h1>Account: {authUser.email}</h1>
+            <h2>Account Type: {authUser.email}</h2>
+            <PasswordChangeForm />
+          </div>
+        )}
+      </AuthUserContext.Consumer>
+      <div>
+        <div className={classes.uploadButtons}>
+          <Button
+            variant="outlined"
+            className={classes.button}
+            style={{ display: 'block' }}
+            onClick={fileSelectedHandler}>
+            <Attachment />
+          </Button>
         </div>
-      )}
-    </AuthUserContext.Consumer>
+        {file ? (
+          <img className={classes.picture} src={file} alt="Picked File" />
+        ) : null}
+      </div>
+    </>
   )
 }
 
 const condition = authUser => !!authUser
 
-export default withAuthorization(condition)(AccountPage)
+export default withAuthorization(condition)(withStyles(styles)(AccountPage))
