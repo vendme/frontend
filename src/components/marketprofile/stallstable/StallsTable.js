5b9eb0cf6d8a2f12bb0f5d3cf969f3fd881975c3
Ashley Owens <ash4741@gmail.com>
2019-07-30 23:44:10 -0400

    Rent Stall Setup

--- src/components/marketprofile/stallstable/StallsTable.js
+++ src/components/marketprofile/stallstable/StallsTable.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import { Tooltip } from '@material-ui/core/'
 import IconButton from '@material-ui/core/IconButton'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 import Table from '@material-ui/core/Table'
@@ -10,6 +9,17 @@ import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const styles = theme => ({
   root: {
@@ -35,49 +45,91 @@ function createData(stall_name, width, length) {
   return { id, stall_name, width, length }
 }
 
+
 function StallsTable(props) {
   const { classes } = props
+  
+  const [open, setOpen] = useState(false);
+  const [duration, setDuration] = useState(1);
+  
+  const handleClickOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
   const data = props.stalls.map(stall => {
     return createData(stall.stall_name, stall.width, stall.length)
   })
   return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell className={classes.cell}>Name</TableCell>
-            <TableCell className={classes.cell}>Width (in)</TableCell>
-            <TableCell className={classes.cell}>Length (in)</TableCell>
-            <TableCell className={classes.cell}>Size (in&sup2;)</TableCell>
-            <TableCell className={classes.cell}>Rent</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map(data => (
-            <TableRow key={data.id}>
-              <TableCell className={classes.cell}>{data.stall_name}</TableCell>
-              <TableCell className={classes.cell}>{data.width}</TableCell>
-              <TableCell className={classes.cell}>{data.length}</TableCell>
-              <TableCell className={classes.cell}>
-                {data.length * data.width}
-              </TableCell>
-              <TableCell className={classes.cell}>
-                <Tooltip
-                  title={'call to rent: ' + props.phone_number}
-                  aria-label={props.phone_number}>
+    <div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.cell}>Name</TableCell>
+              <TableCell className={classes.cell}>Width (in)</TableCell>
+              <TableCell className={classes.cell}>Length (in)</TableCell>
+              <TableCell className={classes.cell}>Size (in&sup2;)</TableCell>
+              <TableCell className={classes.cell}>Rent</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map(data => (
+              <TableRow key={data.id}>
+                <TableCell className={classes.cell}>{data.stall_name}</TableCell>
+                <TableCell className={classes.cell}>{data.width}</TableCell>
+                <TableCell className={classes.cell}>{data.length}</TableCell>
+                <TableCell className={classes.cell}>
+                  {data.length * data.width}
+                </TableCell>
+                <TableCell className={classes.cell}>
                   <IconButton
+                    onClick={handleClickOpen}
                     color="primary"
                     className={classes.button}
                     aria-label="Add to shopping cart">
                     <ShoppingCartIcon />
                   </IconButton>
-                </Tooltip>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+      <DialogTitle id="form-dialog-title">Rent Stall Information</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Please select a time duration from below.
+        </DialogContentText>
+        <form>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="duration">Duration</InputLabel>
+            <Select
+              value={duration}
+              onChange={e => setDuration(e.target.value)}
+              input={<Input id="duration" />}
+            >
+              <MenuItem value={1}>One Day</MenuItem>
+              <MenuItem value={2}>Two Days</MenuItem>
+              <MenuItem value={7}>Seven Days</MenuItem>
+            </Select>
+          </FormControl>
+        </form>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={handleClose} color="primary">
+          Rent
+        </Button>
+      </DialogActions>
+    </Dialog>
+  </div>
   )
 }
 
